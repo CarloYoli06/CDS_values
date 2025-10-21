4fa35f38592c9081699271ad14458f3cc25d2588
MiguelLopezRamirez <miguelangellr14@gmail.com>
2025-10-20 18:48:42 -0700

    Cambios en clase falta estandarizar para que funciones igual

--- src/api/services/cat-labelsvalues-service.js
+++ src/api/services/cat-labelsvalues-service.js
@@ -3,10 +3,61 @@ import mongoose from 'mongoose';
 import { BITACORA, DATA, AddMSG, OK, FAIL } from '../../middlewares/respPWA.handler.js';
 import Etiqueta from '../models/mongodb/etiqueta.js';
 import Valor from '../models/mongodb/valor.js';
+// import { x } from "@sap/cds/lib/utils/tar-lib.js";
+/* EndPoint: localhost:8080/api/inv/crud?ProcessType='GETALL'&LoggedUser=FIBARRAC&DBServer=MongoDB/AzureCosmos  */
+const crudLabelsValues = async (req) => {
+  let bitacora = BITACORA();
+  let data = DATA();
+  
+
+  let {ProcessType} = req.req.query;
+  const {LoggedUser} = req.req.query;
+  const {DBServer} = req.req.query;
+
+  //FIC: get query params
+  //let params = req.req.query;
+   const params = {
+        //WithImagesURL : req.req.query?.WithImagesURL
+        paramsQuery : req.req.query,
+        paramString : req.req.query ? new URLSearchParams(req.req.query).toString().trim() : '',
+        body : req.req.body
+    };
+  //FIC: get params of the service and convert in string
+  //let paramString = req.req.query ? new URLSearchParams(req.req.query).toString().trim() : '';
+  //FIC: get body 
+  //const body = req.req.body;
+
+  //FIC: start fill some properties of the bitacora
+  bitacora.loggedUser = LoggedUser;
+  bitacora.processType = ProcessType;
+  bitacora.dbServer = DBServer;
+
+
+    switch (ProcessType) {
+        case 'CRUD':
+            return await executeCrudOperations(req);
+        case 'GETALL':
+            return await getLabelsValues(req);
+        break;
+        case 'GETID':
+            return await getLabelsValues(req);
+        default:
+            data.messageDEV = `Tipo no válido: ${processType}`;
+            data.messageUSR = "Tipo de operación no reconocida.";
+            bitacora = AddMSG(bitacora, data, 'FAIL', 400);
+            FAIL(bitacora);
+            return req.error(400, data.messageUSR);
+    }
+};
+
 
 const executeCrudOperations = async (req) => {
     let bitacora = BITACORA();
     let data = DATA();
+    const {LoggedUser} = req.req.query;
+    const {DBServer} = req.req.query;
+    data.loggedUser = LoggedUser;
+    data.dbServer = DBServer;
     const session = await mongoose.startSession();
     session.startTransaction();
     const results = [];
@@ -111,7 +162,7 @@ const executeCrudOperations = async (req) => {
         bitacora = AddMSG(bitacora, data, 'FAIL', 500);
         FAIL(bitacora);
         // Return the partial results along with the error
-        return req.error(500, { message: data.messageUSR, results });
+        return req.error(500, bitacora);
     } finally {
         session.endSession();
     }
@@ -120,6 +171,11 @@ const executeCrudOperations = async (req) => {
 const getLabelsValues = async (req) => {
     let bitacora = BITACORA();
     let data = DATA();
+    const {LoggedUser} = req.req.query;
+    const {DBServer} = req.req.query;
+    data.loggedUser = LoggedUser;
+    data.dbServer = DBServer;
+
 
     try {
         data.api = 'getLabelsValues';
@@ -146,26 +202,10 @@ const getLabelsValues = async (req) => {
         data.messageUSR = "Error al obtener las etiquetas y valores.";
         bitacora = AddMSG(bitacora, data, 'FAIL', 500);
         FAIL(bitacora);
-        return req.error(500, data.messageUSR);
+        return req.error(500, data);
     }
 };
 
-const crudLabelsValues = async (req) => {
-    const type = req.req.query?.TYPE;
-    switch (type) {
-        case 'CRUD':
-            return await executeCrudOperations(req);
-        case 'GET':
-            return await getLabelsValues(req);
-        default:
-            let bitacora = BITACORA();
-            let data = DATA();
-            data.messageDEV = `Tipo no válido: ${type}`;
-            data.messageUSR = "Tipo de operación no reconocida.";
-            bitacora = AddMSG(bitacora, data, 'FAIL', 400);
-            FAIL(bitacora);
-            return req.error(400, data.messageUSR);
-    }
-};
+
 
 export default { crudLabelsValues };
