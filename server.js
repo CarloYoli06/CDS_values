2622312f8d9b89aeea9e652f774beef8a3ce2bfa
MiguelLopezRamirez <miguelangellr14@gmail.com>
2025-12-01 22:27:43 -0700

    t

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 import express from "express";
 import cds from "@sap/cds";
 import cors from "cors";
-import mongoose from './src/config/connectToMongoDB.config.js';
+import mongoose from "./src/config/connectToMongoDB.config.js";
 import docEnvX from "./src/config/dotenvXConfig.js";
 import { cosmosDatabase } from "./src/config/conectionToAzureCosmosDB.config.js";
 
 const router = express.Router();
 
 export default async (o) => {
-    try {
-        let app = express();
-        app.express = express;
-        //MALR: 🖖🛑 limit json api response
-        app.use(express.json({ limit: "500kb" }));
-        app.use(cors());
-        app.use(docEnvX.API_URL, router);
-        o.app = app;
-        o.app.httpServer = await cds.server(o);
-        return o.app.httpServer;
-    } catch (error) {
-        console.error('Error strating server:', error);
-        process.exit(1);
-    }
+  o.host = "0.0.0.0";
+  o.port = process.env.PORT || 8080;
+  try {
+    let app = express();
+    app.express = express;
+    //MALR: 🖖🛑 limit json api response
+    app.use(express.json({ limit: "500kb" }));
+    app.use(cors());
+    app.use(docEnvX.API_URL, router);
+    o.app = app;
+    o.app.httpServer = await cds.server(o);
+    return o.app.httpServer;
+  } catch (error) {
+    console.error("Error strating server:", error);
+    process.exit(1);
+  }
 };
